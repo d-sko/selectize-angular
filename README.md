2c0940c7c8b072679b32612cad9c93211003fca4
d-sko <use.this@for.spam>
2013-10-25 18:51:27 +0200

    initial commit, added existing source

---
+++ README.md
@@ -0,0 +1,3 @@
+# selectize-angular
+
+Selectize-angular is an [AngularJS] (http://angularjs.org/) directive for [selectize.js] (http://brianreavis.github.io/selectize.js/) with support for two-way data binding.
---
+++ src/selectize-angular.js
@@ -0,0 +1,56 @@
+'use strict';
+
+angular.module('selectize-angular')
+  .directive('selectize', function ($timeout) {
+    return {
+      restrict: 'A',
+      require: '?ngModel',
+      scope: {
+        seloptions: '=seloptions',
+        ngModel: '=',
+      },
+      link: function postLink(scope, element, attrs, ngModel) {
+        var settings = scope.$eval(attrs.selectize);
+        settings.delimiter = ',';
+        var changeflag = false;
+
+        var safeApply = function ($scope, fn) {
+          var phase = $scope.$root.$$phase;
+          if (phase == '$apply' || phase == '$digest') {
+            if (fn && (typeof (fn) === 'function')) {
+              fn();
+          }
+          } else {
+            $scope.$apply(fn);
+          }
+        };
+
+        var selectizeobj = element.selectize(settings)[0].selectize;
+
+        var onChange = function(value) {
+          safeApply(scope, function() {
+            changeflag = true;
+            scope.ngModel = value.split(',');
+          });
+        }
+
+        scope.$watch('seloptions', function() {
+          safeApply(scope, function() {
+            selectizeobj.clearOptions();
+            for (var i in scope.seloptions)
+              selectizeobj.addOption(scope.seloptions[i]);
+            selectizeobj.refreshOptions();
+          });
+        });
+
+        scope.$watch('ngModel', function(newValue, oldValue) {
+          safeApply(scope, function() {
+            // remove change listener before updating to prevent endless loop
+            selectizeobj.off('change');
+            selectizeobj.setValue(newValue);
+            selectizeobj.on('change', onChange);
+          });
+        });
+      }
+    };
+  });
