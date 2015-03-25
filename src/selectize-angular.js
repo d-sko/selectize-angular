15728aa6ec9a895431da540198ae7b4f3b48ec2c
d-sko <use.this@for.spam>
2015-03-25 15:18:41 +0100

    Rewrite
    Model changes are handled by selectize's events now and the model now
    contains the whole objects instead of only ids.

--- src/selectize-angular.js
+++ src/selectize-angular.js
@@ -4,16 +4,16 @@ angular.module('selectize-angular', [])
   .directive('selectize', function () {
     return {
       restrict: 'A',
-      require: '?ngModel',
       transclude: true,
       scope: {
-        seloptions: '=',
-        ngModel: '=',
+        selection: '=',
+        items: '=',
         render: '=',
         create: '='
       },
       link: function postLink(scope, element, attrs, ngModel) {
         var settings = scope.$eval(attrs.selectize);
+        var externalUpdate = false;
         if (typeof scope.render !== 'undefined' && scope.render !== null) {
           settings.render = scope.render;
         }
@@ -35,29 +35,50 @@ angular.module('selectize-angular', [])
           }
         };
 
-        var selectizeobj = element.selectize(settings)[0].selectize;
+        var selectizeObj = element.selectize(settings)[0].selectize;
 
-        var onChange = function(value) {
-          safeApply(scope, function() {
-            scope.ngModel = value.split(',');
-          });
-        };
+        selectizeObj.on('item_add', function(value) {
+          if (externalUpdate) {
+            return;
+          }
+          if (scope.selection === undefined || !scope.selection instanceof Array) {
+            scope.selection = [];
+          }
+          for (var i in scope.items) {
+            if (scope.items[i][selectizeObj.settings.valueField] === value) {
+              scope.selection.push(scope.items[i]);
+              delete scope.selection[scope.selection.length - 1].$order;
+              break;
+            }
+          }
+        });
 
-        scope.$watch('seloptions', function() {
+        selectizeObj.on('item_remove', function(value) {
+          for (var i in scope.selection) {
+            if (scope.selection[i][selectizeObj.settings.valueField] === value) {
+              scope.selection.splice(i,1);
+              break;
+            }
+          }
+        });
+
+        scope.$watch('items', function() {
           safeApply(scope, function() {
-            selectizeobj.clearOptions();
-            for (var i in scope.seloptions)
-              selectizeobj.addOption(scope.seloptions[i]);
-            selectizeobj.refreshOptions(false);
+            selectizeObj.clearOptions();
+            selectizeObj.addOption(scope.items);
+            selectizeObj.refreshOptions(false);
           });
         });
 
-        scope.$watch('ngModel', function(newValue, oldValue) {
+        scope.$watch('selection', function() {
           safeApply(scope, function() {
-            // remove change listener before updating to prevent endless loop
-            selectizeobj.off('change');
-            selectizeobj.setValue(newValue);
-            selectizeobj.on('change', onChange);
+            externalUpdate = true;
+            selectizeObj.clear();
+            for (var i in scope.selection) {
+              selectizeObj.addItem(scope.selection[i][selectizeObj.settings.valueField], true);
+            }
+            selectizeObj.refreshItems();
+            externalUpdate = false;
           });
         });
       }
